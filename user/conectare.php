bce309716768f77d7775d3de5bfb47a75984f058
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-11-17 11:43:31 +0200

    1.1  Log in + Register DONE 🔐

--- user/conectare.php
+++ user/conectare.php
@@ -6,7 +6,7 @@ $username = 'root';
 /*** mysql password ***/
 $password = '';
 /*** baza de date ***/
-$db = 'gestionare_evenimente';
+$db = 'pst_events';
 /*** se creaza un obiect mysqli ***/
 $mysqli = new mysqli($hostname, $username, $password,$db);
 /* se verifica daca s-a realizat conexiunea */
