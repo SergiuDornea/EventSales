58ddcc66a47b8a909bc1ab4633eac304f3b5054b
Paul Uifalean <uifalean.paul96@gmail.com>
2023-11-18 16:09:11 +0200

    Tickets CRUD

---
+++ stergereTichete.php
@@ -0,0 +1,26 @@
+<?php
+    // Conectare la baza de date:
+    include("conectare.php");
+
+    // Se verifica daca id-ul a fost primit:
+    if (isset($_GET['id']) && is_numeric($_GET['id']))
+    {
+        // Preluam variabila 'id' din URL:
+        $id = $_GET['id'];
+
+        // Stergem inregistrarea cu id = $id:
+        if ($stmt = $mysqli->prepare("DELETE FROM tickets WHERE ID =? LIMIT 1"))
+        {
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $stmt->close();
+        }
+        else
+        {
+            echo "ERROR: Nu se poate executa DELETE.";
+        }
+        $mysqli->close();
+        echo "<div>Inregistrarea a fost stearsa cu succes!</div>";
+    }
+    echo "<p><a href=\"vizualizareTichete.php\">Vizualizare Tichete</a></p>";
+?>
